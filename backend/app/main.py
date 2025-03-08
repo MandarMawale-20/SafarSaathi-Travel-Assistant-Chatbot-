f2ed7fe0387efa74b43cdb575509709906e2ef5d
Avdhut <rokadeavdhut07@gmail.com>
2025-03-08 22:46:36 +0530

    Connected Frontend to Backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import gemini_api
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -11,3 +12,14 @@ def home():
 def chat(prompt: str):
     response = get_gemini_response(prompt)
     return {"response": response}
+
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Allow frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
